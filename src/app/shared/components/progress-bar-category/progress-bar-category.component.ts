04592fbda61f9b2a2fdeb8c89cfb6c2c6687f1a3
Daniel Valle <danielvalleortiz@gmail.com>
2026-03-08 00:06:24 -0500

    Dashboard Components

---
+++ src/app/shared/components/progress-bar-category/progress-bar-category.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-progress-bar-category',
+  templateUrl: './progress-bar-category.component.html',
+  styleUrls: ['./progress-bar-category.component.scss'],
+})
+export class ProgressBarCategoryComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
